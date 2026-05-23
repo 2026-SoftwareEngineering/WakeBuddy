7ecfac93f92b96355dc7fb94bc1c9733ce1f1244
kailey13392 <myjxjx1@naver.com>
2026-05-23 22:38:06 +0900

    [Feat] Alarm 모델 및 서비스 구현

---
+++ wakebuddy-app/src/models/Alarm.ts
@@ -0,0 +1,10 @@
+export type Alarm = {
+  alarmId: string;
+  ownerId: string;
+  creatorId: string;
+  title: string;
+  time: Date;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
